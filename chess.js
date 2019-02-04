1fe8f24b4bee6d36639dd5fb55d3e53f96bb40f7
D'angelo <dangeloshakur@gmail.com>
2019-02-03 18:42:56 -0600

    Completed board generation

--- chess.js
+++ chess.js
@@ -1,21 +1,15 @@
 //
-
-let tileStyle = "";
-
 function generateChessBoard() {
-  for (var i = 0; i < 7; i++) {
-    $('#chessBoard').append(`<div id="r${i}"></div>`);
-    for (var j = 0; j < 7; j++) {
-      $(`#r${i}`).append(`<div id="r${i}c${j}"></div>`);
-    }
-  }
-}
-
-function generateChessBoardStyle() {
-  for (var i = 0; i < 7; i++) {
-    $(`#r${i}`).
-    for(var j = 0; j < 7; j++) {
-      $(`#r${i}c${j}`).
+  for (var i = 0; i < 8; i++) {
+    $('#chessBoard').append(`<div class="column" id="c${i}"></div>`);
+    for (var j = 0; j < 8; j++) {
+      if (((j % 2 == 0) && (i % 2 == 0)) || ((i % 2 == 1) && (j % 2 == 1))) {
+        $(`#c${i}`).append(`<div class="tile" id="c${i}r${j}"></div>`);
+        $(`#c${i}r${j}`).css("background-image", 'url("images/blueTile.jpg")');
+      } else {
+        $(`#c${i}`).append(`<div class="tile" id="c${i}r${j}"></div>`);
+        $(`#c${i}r${j}`).css("background-image", 'url("images/whiteTile.jpg")');
+      }
     }
   }
 }
